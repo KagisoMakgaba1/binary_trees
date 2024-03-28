a0a57153ff2a36cbe24b16500b8b3832abc1b9f0
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-03-28 13:29:46 -0700

    101-binary_tree_levelorder.c

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_levelorder - Performs a level-order traversal of a binary tree
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	int front = 0, rear = 0;
+	binary_tree_t *current;
+
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1024);
+
+	if (queue == NULL)
+		return;
+
+	queue[rear++] = (binary_tree_t *)tree;
+
+	while (front < rear)
+	{
+		current = queue[front++];
+
+		func(current->n);
+
+		if (current->left)
+			queue[rear++] = current->left;
+
+		if (current->right)
+			queue[rear++] = current->right;
+	}
+
+	free(queue);
+}
